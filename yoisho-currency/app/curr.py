5a692d8f1efb44f20cf44c3fe6c984bc87a7f17a
Uli Hitzel <uli.hitzel@gmail.com>
2017-11-18 15:00:02 +0800

    updated version using cherrypy

---
+++ yoisho-currency/app/curr.py
@@ -0,0 +1,42 @@
+from bottle import Bottle, request
+import json
+from random import randint
+from datetime import datetime
+
+app = Bottle()
+
+@app.get('/get_currency')
+def get_currency():
+
+	currency = request.query['currency']
+        clist_buy={'USD':'389', 'SGD':'281'}
+        clist_sell={'USD':'489', 'SGD':'381'}
+
+        rcur={}
+
+        rand1 = randint(100,200)
+        rand2 = randint(100,200)
+
+        try:
+                rcur['buy'] = str(clist_buy[currency]) + "." + str(rand1)
+        except:
+                rcur['buy'] = -1
+
+        try:
+                rcur['sell'] = str(clist_sell[currency]) + "." + str(rand2)
+        except:
+                rcur['sell']=-1
+
+        rcur['timestamp']=str(datetime.now())
+
+	return dict(data=rcur)
+
+
+
+@app.get('/swagger')
+def swagger():
+
+	swagger = '''{   "swagger" : "2.0",   "host" : "",   "basePath" : "/currency",   "schemes" : [ "http" ],   "paths" : {     "/get_currency" : {       "get" : {         "description" : "",         "operationId" : "get_currency",         "produces" : [ "application/json" ],         "parameters" : [ {           "description" : "The desired currency",           "required" : true,           "in" : "query",           "name" : "currency",           "type" : "string"         } ],         "responses" : {           "default" : {             "description" : "successful operation"           }         }       }     }   },   "info" : {     "title" : "Yoisho Currency Exchange",     "description" : "",     "version" : "1.0"   },   "x-axway" : {     "corsEnabled" : true,     "basePaths" : [ "" ],     "serviceType" : "rest",     "deprecated" : false,     "tags" : { }   } }'''
+
+	return swagger
+
