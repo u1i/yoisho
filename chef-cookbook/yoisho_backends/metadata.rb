185a60f518220417618171669b99fb9729b63e97
Uli Hitzel <uli.hitzel@gmail.com>
2018-07-29 14:00:39 +0000

    changed

--- chef-cookbook/yoisho_backends/metadata.rb
+++ chef-cookbook/yoisho_backends/metadata.rb
@@ -1,20 +1,9 @@
 name 'yoisho_backends'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Uli Hitzel'
+maintainer_email 'uli.hitzel@gmail.com'
+license 'MIT License'
 description 'Installs/Configures yoisho_backends'
 long_description 'Installs/Configures yoisho_backends'
 version '0.2.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/yoisho_backends/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/yoisho_backends'
+depends 'docker'
