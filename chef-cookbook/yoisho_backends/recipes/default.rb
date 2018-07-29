462916427d75e7dca128bfbe18aa7c70aecde2b2
Uli Hitzel <uli.hitzel@gmail.com>
2018-07-29 14:04:14 +0000

    changed

--- chef-cookbook/yoisho_backends/recipes/default.rb
+++ chef-cookbook/yoisho_backends/recipes/default.rb
@@ -5,5 +5,17 @@
 # Author: Uli Hitzel - uli.hitzel@gmail.com
 
 # Install Docker
-
 docker_installation 'default'
+
+# Currency
+docker_image 'u1ih/yoisho-currency' do
+  tag 'latest'
+  action :pull
+  notifies :redeploy, 'docker_container[yoishocurrency]'
+end
+
+docker_container 'yoishocurrency' do
+  repo 'u1ih/yoisho-currency'
+  tag 'latest'
+  port '8080:8080'
+end
