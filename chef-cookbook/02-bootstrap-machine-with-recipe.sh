0efcfc0d1bd71747c7f5c700f9cba86b0d393455
Uli Hitzel <uli.hitzel@gmail.com>
2018-07-29 14:14:55 +0000

    changed

---
+++ chef-cookbook/02-bootstrap-machine-with-recipe.sh
@@ -0,0 +1,8 @@
+ip=52.221.240.102
+nodename=node${RANDOM}-ubuntu
+sshuser=ubuntu
+ssh_key=$HOME/.ssh/uh-2017-06.pem
+
+knife bootstrap $ip --ssh-user $sshuser --sudo --identity-file $ssh_key --node-name $nodename --run-list 'recipe[yoisho_backends]'
+
+knife node show $nodename
