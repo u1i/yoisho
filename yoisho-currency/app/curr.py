d509862ae908f4929b1356ddc852da61c2c52843
Uli Hitzel <uli.hitzel@gmail.com>
2019-03-26 02:44:28 +0800

    cors headers for currency

--- yoisho-currency/app/curr.py
+++ yoisho-currency/app/curr.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, request
+from bottle import Bottle, request, response
 import json
 from random import randint
 from datetime import datetime
@@ -33,8 +33,8 @@ def get_currency():
                 rcur['sell']=-1
 
         rcur['timestamp']=str(datetime.now())
-
-	#return dict(data=rcur)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
 	return dict(rcur)
 
 
@@ -42,14 +42,19 @@ def get_currency():
 @app.get('/swagger')
 def swagger():
 
-	swagger = '''{   "swagger" : "2.0",   "host" : "",   "basePath" : "",   "schemes" : [ "http" ],   "paths" : {     "/get_currency" : {       "get" : {         "description" : "",         "operationId" : "get_currency",         "produces" : [ "application/json" ],         "parameters" : [ {           "description" : "The desired currency",           "required" : true,           "in" : "query",           "name" : "currency",           "type" : "string"         } ],         "responses" : {           "default" : {             "description" : "successful operation"           }         }       }     }   },   "info" : {     "title" : "Yoisho Currency Exchange",     "description" : "",     "version" : "1.0"   },   "x-axway" : {     "corsEnabled" : true,     "basePaths" : [ "" ],     "serviceType" : "rest",     "deprecated" : false,     "tags" : { }   } }'''
+    swagger = '''{   "swagger" : "2.0",   "host" : "",   "basePath" : "",   "schemes" : [ "http" ],   "paths" : {     "/get_currency" : {       "get" : {         "description" : "",         "operationId" : "get_currency",         "produces" : [ "application/json" ],         "parameters" : [ {           "description" : "The desired currency",           "required" : true,           "in" : "query",           "name" : "currency",           "type" : "string"         } ],         "responses" : {           "default" : {             "description" : "successful operation"           }         }       }     }   },   "info" : {     "title" : "Yoisho Currency Exchange",     "description" : "",     "version" : "1.0"   },   "x-axway" : {     "corsEnabled" : true,     "basePaths" : [ "" ],     "serviceType" : "rest",     "deprecated" : false,     "tags" : { }   } }'''
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
 
-	return swagger
+    return swagger
 
 
 @app.get('/fx/swagger')
 def swagger():
 
-	swagger = '''{   "swagger" : "2.0",   "host" : "",   "basePath" : "/fx",   "schemes" : [ "http" ],   "paths" : {     "/currency" : {       "get" : {         "description" : "",         "operationId" : "get_currency",         "produces" : [ "application/json" ],         "parameters" : [ {           "description" : "The desired currency",           "required" : true,           "in" : "query",           "name" : "currency",           "type" : "string"         } ],         "responses" : {           "default" : {             "description" : "successful operation"           }         }       }     }   },   "info" : {     "title" : "Yoisho Currency Exchange",     "description" : "",     "version" : "1.0"   },   "x-axway" : {     "corsEnabled" : true,     "basePaths" : [ "" ],     "serviceType" : "rest",     "deprecated" : false,     "tags" : { }   } }'''
+    swagger = '''{   "swagger" : "2.0",   "host" : "",   "basePath" : "/fx",   "schemes" : [ "http" ],   "paths" : {     "/currency" : {       "get" : {         "description" : "",         "operationId" : "get_currency",         "produces" : [ "application/json" ],         "parameters" : [ {           "description" : "The desired currency",           "required" : true,           "in" : "query",           "name" : "currency",           "type" : "string"         } ],         "responses" : {           "default" : {             "description" : "successful operation"           }         }       }     }   },   "info" : {     "title" : "Yoisho Currency Exchange",     "description" : "",     "version" : "1.0"   },   "x-axway" : {     "corsEnabled" : true,     "basePaths" : [ "" ],     "serviceType" : "rest",     "deprecated" : false,     "tags" : { }   } }'''
 
-	return swagger
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+
+    return swagger
